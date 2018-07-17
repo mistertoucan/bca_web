ab3c72f057585978e4853fe17af2a0226a0b4447
anthonylekan <anthonylekan25@gmail.com>
2018-07-17 09:22:23 -0400

    MySQL Setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_mysqldb import MySQL
+
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
-    return "Hello world!"
+    return "Hello World!"
