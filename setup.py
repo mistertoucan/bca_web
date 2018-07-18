9cc93cacecbf52946c8d69a300b506e80b1f6b06
anthonylekan <anthonylekan25@gmail.com>
2018-07-17 21:19:59 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='bca_web',
+    version='1.0',
+    packages=['app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['Flask',
+                      'mysqlclient',
+                      'flask_mysqldb',
+                      'flask_login']
+    )
