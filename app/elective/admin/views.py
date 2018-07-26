b0c73a063de90e6b0234b3149e63c3fc5f52c185
anthonylekan <anthonylekan25@gmail.com>
2018-07-26 11:38:24 -0400

    Fixed Elective Auth + Added '/flask-apps/' icon reg

--- app/elective/admin/views.py
+++ app/elective/admin/views.py
@@ -2,13 +2,13 @@ from app.elective.admin import admin_mod
 
 from app.shared.controllers import requires_token
 
-from flask import g, redirect
+from flask import g, redirect, url_for
 
 @admin_mod.before_request
 @requires_token
 def check_teacher():
-    if g.user.get_type_code() != 'TCH':
-        return redirect('elective')
+    if g.user.get_type_code() != 'ADM':
+        return redirect(url_for('elective'))
 
 @admin_mod.route('/')
 def index():
--- app/elective/student/views.py
+++ app/elective/student/views.py
@@ -2,13 +2,13 @@ from app.elective.student import student_mod
 
 from app.shared.controllers import requires_token
 
-from flask import g, redirect
+from flask import g, redirect, url_for
 
 @student_mod.before_request
 @requires_token
 def check_teacher():
-    if g.user.get_type_code() != 'TCH':
-        return redirect('elective')
+    if g.user.get_type_code() != 'STD':
+        return redirect(url_for('student'))
 
 @student_mod.route('/')
 def index():
