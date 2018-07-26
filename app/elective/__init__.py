b9f7a69a88f8437c78ac15febf5d37e02765101c
anthonylekan <anthonylekan25@gmail.com>
2018-07-26 09:28:51 -0400

    Changed elective app name

--- app/elective/__init__.py
+++ app/elective/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-elective_mod = Blueprint('elective', __name__, url_prefix='/elective')
+elective_mod = Blueprint('elective', __name__, url_prefix='/elective_enroll')
 
 import app.elective.views
