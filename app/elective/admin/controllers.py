27f1fbf5bed69c56bc8e7aec131c5f3b8cfcfa09
ansmor <ansmor21@bergen.org>
2019-02-06 09:15:03 -0500

    electives/admin - signup dates (v1) + signup dates query (v2)

--- app/elective/admin/controllers.py
+++ app/elective/admin/controllers.py
@@ -9,8 +9,12 @@ def update_enroll_date(newDate, trimester_id, grade):
     pass
 
 def get_signup_dates():
-    result = query_one(DB.ELECTIVE, "SELECT * "
-                                    "FROM signup_dates ")
+    signup_dates = []
+    results = query(DB.ELECTIVE, "SELECT * FROM signup_dates ")
+
+    for result in results:
+        signup_dates.append(SignupDate(result[0], result[1], result[2], result[3], result[4], result[5]))
+
     return result
 
 
